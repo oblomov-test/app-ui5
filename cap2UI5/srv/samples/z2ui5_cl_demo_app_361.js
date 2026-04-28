115114cb9fa55372ee5dba82d39de0e26e2d5c3b
oblomov-dev <102328295+oblomov-dev@users.noreply.github.com>
2026-04-28 19:11:07 -0300

    New ui5 v2 (#9)
    * update app
    
    * mirror abap2UI5
    
    * mirror abap2UI5
    
    * fix versions
    
    * add launchpad
    
    * Delete .DS_Store
    
    ---------
    
    Co-authored-by: developer <lars.kaldewey@agilita.de>

---
+++ cap2UI5/srv/samples/z2ui5_cl_demo_app_361.js
@@ -0,0 +1,37 @@
+const z2ui5_if_app = require("../z2ui5/02/z2ui5_if_app");
+const z2ui5_cl_xml_view = require("../z2ui5/02/z2ui5_cl_xml_view");
+
+class z2ui5_cl_demo_app_361 extends z2ui5_if_app {
+
+  async main(client) {
+
+    if (client.check_on_init()) {
+
+      const view = z2ui5_cl_xml_view.factory();
+      view.Shell()
+        .Page({
+          title:          `abap2UI5 - System Logout`,
+          navButtonPress: client._event_nav_app_leave(),
+          showNavButton:  client.check_app_prev_stack(),
+        })
+        .MessageStrip({
+          class:    `sapUiMediumMargin`,
+          showIcon: true,
+          text:     `Trigger SYSTEM_LOGOUT on the client. Inside a Fiori Launchpad the shell container handles the sign-out; otherwise the app navigates to the ICF logoff endpoint.`,
+          type:     `Information`,
+        })
+        .Button({
+          class: `sapUiSmallMargin`,
+          icon:  `sap-icon://log`,
+          text:  `Logout now`,
+          type:  `Reject`,
+          press: client._event_client(client.cs_event.SYSTEM_LOGOUT),
+        });
+      client.view_display(view.stringify());
+
+    }
+
+  }
+}
+
+module.exports = z2ui5_cl_demo_app_361;
